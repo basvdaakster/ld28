e1a1769d33c05261441043017c40eb755da853c0
Bas van den Aakster <bas.who.else@live.nl>
2013-12-09 12:28:31 +0100

    Initial Project.

---
+++ LD28/src/com/aakster/ld28/LD28.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.aakster.ld28;
+
+/**
+ *
+ * @author Bas van den Aakster
+ */
+public class LD28 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+
+}
